ed9738e39cf31fb03a46cce870e77556a15528a6
alejoboga-io <alejo.boga@pendo.io>
2024-10-24 18:06:17 -0300

    close connection on end

--- app/main.ts
+++ app/main.ts
@@ -1,21 +1,15 @@
 import * as net from 'net';
 
-const redisCommands = {
-	PING: 'PING',
-};
-
 console.log('Logs from your program will appear here!');
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
 	console.log('Client Connected');
 
-	connection.on('data', (data) => {
-		const parsedData = data.toString().trim();
-		const messageType = parsedData[0];
-
-		if (messageType === '*') {
-			connection.write('+PONG\r\n');
-		}
+	connection.on('data', () => {
+		connection.write('+PONG\r\n');
+	});
+	connection.on('close', () => {
+		connection.end();
 	});
 });
 
